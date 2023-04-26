205ce0d3f22bd9fbd8d658bb1d73936d63c5d487
vimalachandran <cvimala812@gmail.com>
2023-04-26 19:11:09 +0530

    spotify

---
+++ src/com/increment/Increment.java
@@ -0,0 +1,13 @@
+package com.increment;
+
+public class Increment {
+	public static void main(String[]args) {
+		for(int i=0;i<=5;i++) {
+			for(int j=0;j<=5;j--) {
+				System.out.print(i);
+				System.out.println(j);
+			}
+		}
+	}
+
+}
